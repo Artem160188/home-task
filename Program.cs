270607431b6d9d207d8a08515c357dc12b69a9d7
Artem160188 <118046594+Artem160188@users.noreply.github.com>
2022-11-26 22:24:05 +0300

    task3333

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num%2;
+if (result == 0)
+{
+    Console.WriteLine("Число является четным");
+
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+    }
