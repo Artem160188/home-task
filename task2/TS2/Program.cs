9adc4f7b7962f6e994b61a8da7db1a4d2149417d
Artem <118046594+Artem160188@users.noreply.github.com>
2022-11-26 22:36:19 +0300

    Add files via upload

---
+++ task2/TS2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите 1ое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2ое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввкдите 3е число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if 
+
+   ( num2 > max) 
+    max = num2;
+
+if 
+   
+     (num3>max)
+    max=num3;
+   
+Console.WriteLine($"Максимальное из введенных чисел{max}");
+
+
+
